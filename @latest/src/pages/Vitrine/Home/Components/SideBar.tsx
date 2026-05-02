d783ecab9f641ff714029f3c0ed18efd488690e9
SuelleMaciel <suellemaciel7@gmail.com>
2026-05-02 16:34:41 -0300

    style: unificando estilos entre pagina do gestor e vitrine

--- @latest/src/pages/Vitrine/Home/Components/SideBar.tsx
+++ @latest/src/pages/Vitrine/Home/Components/SideBar.tsx
@@ -2,10 +2,18 @@ import { Link } from 'react-router-dom';
 import styles from './SideBar.module.css';
 
 export function Sidebar() {
+  const isLogado = localStorage.getItem('usuarioLogado') === 'true';
+
+  const handleLogout = () => {
+    localStorage.removeItem('usuarioLogado');
+    
+    window.location.href = '/'; 
+  };
+
   return (
     <aside className={styles.sidebar}>
       <div className={styles.logo}>
-        <img height={130} width={130} src="../src/assets/logo-temporaria-exemplo.png"></img>
+        <img height={130} width={130} src="../src/assets/logo-temporaria-exemplo.png" alt="Logo" />
         <h2>Pitchmatching GovTech</h2>
       </div>
       
@@ -14,9 +22,27 @@ export function Sidebar() {
           Vitrine (Home)
         </Link>
         
-        <Link to="/login" className={styles.link}>
-          Login / Acesso
-        </Link>
+        {isLogado ? (
+          <>
+            <Link to="/gestor" className={styles.link}>
+              Dashboard
+            </Link>
+            
+            <Link to="/gestor/nova-demanda" className={styles.link}>
+              Nova Demanda
+            </Link>
+            
+            <button 
+            onClick={handleLogout} 
+            className={styles.btnLogout}>
+            Sair
+            </button>
+          </>
+        ) : (
+          <Link to="/login" className={styles.link}>
+            Login / Acesso
+          </Link>
+        )}
       </nav>
     </aside>
   );
